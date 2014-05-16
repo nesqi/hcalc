aa59d8645289f200f15a0627d03c617bc36bc8ba
Erik Jonsson <erik@nesqi.se>
2014-05-16 07:19:10 +0200

    Added todo-list, refined merge in simplify, added unary minus to Values.

--- calculator.hs
+++ calculator.hs
@@ -5,37 +5,21 @@ import Data.Ratio
 import Data.List
 import qualified Data.Map.Lazy as ML
 
+--TODO: 
+-- Nice printouts
+-- Evaluation (Simplification + evaluation)
+-- Tests (quick check)
+-- Print out mode (Dec, Hex, Oct, ...)
+-- Variables
+-- Functions
+-- Modulo operator
+
 data Expr = Value Rational
           | Neg Expr
           | Inv Expr
           | Op String Expr [ Expr ]
           | UnOp String Expr
     deriving (Eq, Show, Ord)
-
-isValue :: Expr -> Bool
-isValue (Value _) = True
-isValue _ = False
-
-isInvValue :: Expr -> Bool
-isInvValue (Inv (Value _)) = True
-isInvValue _ = False
-
-isNegValue :: Expr -> Bool
-isNegValue (Neg (Value _)) = True
-isNegValue _ = False
-
-isOp :: Expr -> Bool
-isOp (Op _ _ _) = True
-isOp _ = False
-
-isUnOp :: Expr -> Bool
-isUnOp (UnOp _ _) = True
-isUnOp _ = False
-
---instance Show Expr where
---    show (Value i) = show i
---    show (BinOp o a b) = "(" ++ show a ++ o ++ show b ++ ")"
---    show (UnOp f a) = f ++ "(" ++ show a ++ ")"
 
 transform :: (Expr -> Expr) -> Expr -> Expr
 transform f old = let new = case old of
@@ -61,6 +45,11 @@ flatten = transform f
                                (Op o x y) -> if o == op then (x:extract op y) else [(Op o x y)]
                                x          -> [x]
 
+data Ent a = CombineE Expr a -- Expression that should be combined
+           | CombineI a      -- No expression needed just combine
+           | Pass Expr       -- Don't combine just pass along
+    deriving (Eq)
+
 simplify :: Expr -> Expr
 simplify = transform f
     where f (Op "^" (Value a) [(Value b)]) = case (b >= 0, (denominator b) == 1) of
@@ -72,22 +61,26 @@ simplify = transform f
           -- Addition of 0
           f (Op "+" (Value 0) a) = toOp "+" a
           f (Op "+" a [Value 0]) = a
-       
+          f (Inv (Value a)) = Value $ 1/a
           f (Neg (Value a)) = Value $ -a
 
-          f (Op "+" a b) = toOp "+" $ merge (+) lowerT liftT (a:b)
+          f (Op "+" a b) = toOp "+" $ merge lowerT liftT (a:b)
                            where
-                               liftT (Op "*" (Value x) [y]) = (y, Just x)
-                               liftT (Op "*" y [Value x]) = (y, Just x)
-                               liftT (Value x) = (Value 1, Just x)
-                               liftT (Neg x) = (x, Just (-1))
-                               liftT (x) = (x, Just 1)
+                               liftT (Op "*" (Value x) [y]) = [CombineE y (x+)]
+                               liftT (Op "*" y [Value x]) = [CombineE y (x+)]
+                               liftT (Value x) = [CombineI (x+)]
+                               liftT (Neg x) = [CombineE x (-1+)]
+                               liftT (x) = [CombineE x (1+)]
                                
-                               lowerT (_, Just 0)  = []
-                               lowerT ((Value 1), Just x)  = [(Value x)]
-                               lowerT (x, Just (-1)) = [Neg x]
-                               lowerT (x, Just 1)  = [x]
-                               lowerT (x, Just y)  = [(Op "*" (Value $ y) [x])]
+                               lowerT (Pass x) = [x]
+                               lowerT (CombineI n)
+                                   | (n 0) == 0 = []
+                                   | otherwise = [Value (n 0)]
+                               lowerT (CombineE x n)
+                                   | (n 0) == 0 = []
+                                   | (n 0) == -1 = [Neg x]
+                                   | (n 0) == 1 = [x]
+                                   | otherwise = [Op "*" (Value $ n 0) [x]]
 
           -- Reduce integer devisions
           f (Op "/" (Value a) [Value b]) = (Value $ a / b)
@@ -98,61 +91,43 @@ simplify = transform f
           f (Op "*" (Value 0) _) = Value 0
           f (Op "*" _ [Value 0]) = Value 0
 
-          f (Op "*" a b) = toOp "*" $ mergeValues (a:b)
+          f (Op "*" a b) = toOp "*" $ merge lowerE liftE (a:b)
                            where
-                               -- Merge values
-                               mergeValues = merge (*) lowerVal liftVal
-                               
-                               liftVal (Value x)       = (Value 1, Just x)
-                               liftVal (Inv (Value x)) = (Value 1, Just $ 1 / x)
-                               liftVal (x) = (x, Nothing)
-                               
-                               lowerVal (_, Just 0) = []
-                               lowerVal ((Value 1), Just x)  = [(Value x)]
-                               lowerVal (x, Nothing)  = [x]
-                               
-                               -- Merge Expressions
-                               mergeExpr = merge (+) lowerExpr liftExpr
-                               
-                               liftExpr (Value x)       = (Value x, Nothing)
-                               liftExpr (Inv (Value x)) = (Inv (Value x), Nothing)
-                               liftExpr (Op "*" (Value x) [y]) = (y, Just x)
-                               liftExpr (Op "*" y [Value x]) = (y, Just x)
-                               liftExpr (Neg x) = (x, Just (-1))
-                               liftExpr (x) = (x, Just 1)
-                               
-                               lowerExpr (_, Just 0)  = []
-                               lowerExpr ((Value 1), Just x)  = [(Value x)]
-                               lowerExpr (x, Just (-1)) = [Neg x]
-                               lowerExpr (x, Just 1)  = [x]
-                               lowerExpr (x, Just y)  = [(Op "^" x [Value $ y])]
-                               lowerExpr (x, Nothing)  = [x]
-          
+                               -- Values
+                               liftE (Op "*" (Value x) [y]) = [CombineI (x*), Pass y]
+                               liftE (Op "*" y [Value x])   = [CombineI (x*), Pass y] 
+                               liftE (Neg x) = [CombineI ((-1)*)] ++ liftE x
+                               liftE (Value x)       = [CombineI (x*)]
+                               liftE (Inv (Value x)) = [CombineI ((1/x)*)]
+                               -- Expr
+                               liftE (Op "^" (Value x) [y]) = [CombineE y (x+)]
+                               liftE (Op "^" y [Value x])   = [CombineE y (x+)] 
+                               liftE (Inv x) = [CombineE x (-1+)]
+                               liftE (x) = [CombineE x (1+)]
+
+                               lowerE (Pass x)  = [x]
+                               lowerE (CombineI x)  = [Value $ x 1]
+                               lowerE (CombineE x n) 
+                                   | (n 0) == 0 = []
+                                   | (n 0) == 1 = [x]
+                                   | otherwise  = [Op "^" x [Value $ n 0]]
           f x = x
 
-          merge cOp down up l = let (process, pass) = partition hasValue $ fmap up l
-                                    merged = ML.toList . ML.fromListWith (liftM2 cOp) $ process
-                                    hasValue (_, Just _) = True
-                                    hasValue _           = False
-                                in foldl1 (++) $ fmap down $ merged ++ pass
+          merge down up l = let (pass, process) = partition passQ $ foldl1 (++) $ fmap up l
+                                merged = fmap (toComb) $ ML.toList . ML.fromListWith (.) $ fmap (toPair) process
+                                passQ (Pass _) = True
+                                passQ _        = False
+                                toPair (CombineI n) = ([], n)
+                                toPair (CombineE e n) = ([e], n)
+                                toComb ([], n) = CombineI n
+                                toComb ([e], n) = CombineE e n
+                            in foldl (++) [] $ fmap down $ merged ++ pass
           
           toOp op lst = case lst of
                             [] -> Value 0
                             [x] -> x
                             (x:xs) -> (Op op x xs)
 
-
--- a/b*c -> (a*c)/b
---simplify (BinOp "*" (BinOp "/" a b) c) = simplify $ BinOp "/" (simplify $ BinOp "*" a c) (simplify b)
--- a/b+c -> (a+c*b)/b | b, c are Values
---simplify (BinOp "+" (BinOp "/" a (Value b)) (Value c)) = simplify $ BinOp "/" (simplify $ BinOp "+" a (Value $ b * c)) (Value b)
---simplify (BinOp "+" (Value c) (BinOp "/" a (Value b))) = simplify $ BinOp "/" (simplify $ BinOp "+" a (Value $ b * c)) (Value b)
--- a/b+c/d -> (a*d+c*b)/b*d | a, b, c, d are Values
---simplify (BinOp "+" (BinOp "/" (Value a) (Value b)) (BinOp "/" (Value c) (Value d))) = 
---    simplify $ BinOp "/" (simplify $ BinOp "+" (Value $ a * d) (Value $ c * b)) (Value $ b * d)
---simplify (BinOp "*" (Value a) (Value b)) = (Value $ a * b)
---simplify (BinOp "*" b (Value a)) = (BinOp "*" e (Value $ a*v)) where (e, v) = collect b
---simplify (BinOp "*" (Value a) b) = (BinOp "*" (Value $ v*a) e) where (e, v) = collect b
 
 inLine :: Parser [[Expr]]
 inLine = endBy statements newline
@@ -182,7 +157,9 @@ fun = do f <- function
          return (UnOp f a)
 
 valueP :: Parser Expr 
-valueP = (try $ string "0x" >> integerP 16)
+valueP = (char '-' >> do v <- valueP
+                         return (Neg v))
+     <|> (try $ string "0x" >> integerP 16)
      <|> (try $ string "0o" >> integerP 8)
      <|> (try $ string "0b" >> integerP 2)
      <|> integerP 10
@@ -252,6 +229,5 @@ main = do print "HCalc"
                  case parseLine (l ++ "\n") of
                      (Right es) -> do let e = (head $ head es)
                                       print $ e
-                                      print $ flatten e
                                       print $ simplify $ flatten e
                      (Left err) -> print $ err
